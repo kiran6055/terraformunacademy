f9a3103445c8de3a4bd74ce25ca85ca48c27198d
kiran6055 <kirandevopskumar@gmail.com>
2023-09-15 13:33:44 +0530

    all

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,5 @@
 resource "aws_instance" "web" {
-<<<<<<< HEAD
   ami           = "ami-05552d2dcf89c9b24"
-=======
-  ami           = data.aws_ami.ubuntu.id
->>>>>>> 3f95697f88c971bc023796f824032c225fd5e2ce
   instance_type = "t3.micro"
 
   tags = {
